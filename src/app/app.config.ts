33069989098e41d4a6197ad33320ac9c860d6a0a
soheil <aliasgharysoheil02@gmail.com>
2025-10-11 23:34:36 +0330

    fix : fix bug in ui button

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,31 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { 
+  SocialAuthServiceConfig,
+  GoogleLoginProvider 
+} from '@abacritt/angularx-social-login';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              'YOUR_CLIENT_ID.apps.googleusercontent.com', // جایگزین کن
+              {
+                oneTapEnabled: false,
+                scopes: 'email profile openid'
+              }
+            )
+          }
+        ]
+      } as SocialAuthServiceConfig,
+    }
+  ]
 };
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SocialAuthService, SocialUser , GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
+  standalone: true,
+  imports: [CommonModule , GoogleSigninButtonModule],
 })
-export default class LoginComponent {
+export default class LoginComponent implements OnInit {
+  user: SocialUser | null = null;
 
+  constructor(private authService: SocialAuthService) {}
+
+  ngOnInit(): void {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      console.log('User:', user);
+      if (user) {
+        alert('Login successful! Welcome ' + user.name);
+      }
+    });
+  }
 }
