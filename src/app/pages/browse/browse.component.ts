b7c8385bf6779506e6ce547ee2d550da4a3fbce8
soheil <aliasgharysoheil02@gmail.com>
2025-10-18 00:04:20 +0330

    fix bug

--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../core/components/header/header.component';
 import { BannerComponent } from '../../core/components/banner/banner.component';
+import { MovieCarouselComponent } from '../../shared/components/movie-carousel/movie-carousel.component';
 
 @Component({
   selector: 'app-browse',
   standalone: true,
-  imports: [HeaderComponent, BannerComponent],
+  imports: [HeaderComponent, BannerComponent , MovieCarouselComponent],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.scss'
 })
---
+++ src/app/shared/components/movie-carousel/movie-carousel.component.ts
@@ -0,0 +1,39 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import Swiper from 'swiper'
+@Component({
+  selector: 'app-movie-carousel',
+  imports: [],
+  templateUrl: './movie-carousel.component.html',
+  styleUrl: './movie-carousel.component.scss'
+})
+export class MovieCarouselComponent {
+@ViewChild('swiperContainer') swiperContainer !: ElementRef;
+
+ initSwaper(){
+   return new Swiper(this.swiperContainer.nativeElement , {
+    slidesPerView: 3,
+    slidesPerGroup : 2,
+    loop : true,
+    centeredSlides : true,
+    breakpoints : {
+      425 : {
+        slidesPerView : 1,
+        slidesPerGroup : 1
+      },
+      640 : {
+        slidesPerView : 1,
+        slidesPerGroup : 1
+      },
+      768 : {
+        slidesPerView : 2,
+        slidesPerGroup : 2
+      },
+      1024 : {
+        slidesPerView : 3,
+        slidesPerGroup : 2
+      }
+    }
+   })
+ }
+
+}
